6ee097b1cb9876f8122592f930a30493bc53f98e
Adam Dierkens <Adam_Dierkens@intuit.com>
2022-07-15 16:57:46 -0700

    Add support for publishing npm modules

--- scripts/delete-old-prerelease.js
+++ scripts/delete-old-prerelease.js
@@ -1,29 +1,27 @@
-const { execSync } = require("child_process");
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-await-in-loop */
+const { execSync } = require('child_process');
 
 const getTags = () => {
-  const tags = execSync("git tag --sort=-creatordate", { encoding: "utf8" });
+  const tags = execSync('git tag --sort=-creatordate', { encoding: 'utf8' });
   return tags
-    .split("\n")
+    .split('\n')
     .map((t) => t.trim())
-    .filter((tag) => tag.includes("-next."));
+    .filter((tag) => tag.includes('-next.'));
 };
 
-/**
- * @implements {import('@auto-it/core').IPlugin}
- */
 class DeleteOldPrereleasePlugin {
-  name = "delete-old-prerelease";
+  name = 'delete-old-prerelease';
 
-  /**
-   *
-   * @param {Auto} auto
-   */
   apply(auto) {
     auto.hooks.next.tapPromise(this.name, async ({ dryRun }) => {
       const existingGitTags = getTags();
 
-      for (let tag of existingGitTags) {
-        if (!dryRun) {
+      for (const tag of existingGitTags) {
+        if (dryRun) {
+          auto.logger.log.info(`Dry run: would delete release for tag: ${tag}`);
+        } else {
           try {
             const ghRelease = await auto.git.github.repos.getReleaseByTag({
               owner: auto.git.options.owner,
@@ -43,30 +41,6 @@ class DeleteOldPrereleasePlugin {
               `Failed to delete remote release for tag: ${tag}`
             );
           }
-
-          try {
-            auto.logger.log.info(`Deleting remote tag: ${tag}`);
-
-            await auto.git.github.git.deleteRef({
-              owner: auto.git.options.owner,
-              repo: auto.git.options.repo,
-              ref: `tags/${tag}`,
-            });
-          } catch (e) {
-            auto.logger.log.warn(`Failed to delete remote tag: ${tag}`);
-          }
-
-          try {
-            auto.logger.log.info(`Deleting local tag: ${tag}`);
-
-            execSync(`git tag -d ${tag}`);
-          } catch (e) {
-            auto.logger.log.warn(`Failed to delete local tag: ${tag}`);
-          }
-
-          auto.logger.log.info(`Deleting tag: ${tag}`);
-        } else {
-          auto.logger.log.info(`Dry run: would delete release for tag: ${tag}`);
         }
       }
     });
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
+# See https://github.com/bazelbuild/rules_nodejs/blob/stable/scripts/publish_release.sh 
 
 set -u -e -o pipefail
 
-readonly DEPLOY_LABELS=`bazel query --output=label "kind('deploy_.* rule', ${2:-//...})"`
+readonly PKG_NPM_LABELS=`bazel query --output=label 'kind("pkg_npm rule", //...) - attr("tags", "\[.*do-not-publish.*\]", //...)'`
+NPM_TAG=canary
 
-# skip pkg releases for now
+# Called by auto -- `release` for normal releases or `snapshot` for canary/next.
+readonly RELEASE_TYPE=${1:-snapshot}
+readonly CURRENT_BRANCH=`git symbolic-ref --short HEAD`
 
-# for pkg in $DEPLOY_LABELS ; do
-#   bazel run $pkg -- $1
-# done
+if [ "$RELEASE_TYPE" == "snapshot" ] && [ "$CURRENT_BRANCH" == "main" ]; then
+  NPM_TAG=next
+elif [ "$RELEASE_TYPE" == "release" ] && [ "$CURRENT_BRANCH" == "main" ]; then
+  # Releases off the main branch are for older majors. 
+  # Don't want to bump the latest tag for those
+
+  NPM_TAG=latest
+fi
+
+for pkg in $PKG_NPM_LABELS ; do
+  bazel run --config=release -- ${pkg}.publish --access public --tag ${NPM_TAG}
+done
 
 # Running this here because it will still have the pre-release version in the VERSION file before auto cleans it up
 bazel run //docs:deploy_docs
