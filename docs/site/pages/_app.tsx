a11f0b7fb7856c07d165c1d713c71be6255fe48a
Adam Dierkens <Adam_Dierkens@intuit.com>
2022-07-19 16:09:01 -0700

    Analytics for docs

--- docs/site/pages/_app.tsx
+++ docs/site/pages/_app.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
-import type { AppProps } from 'next/app';
+import type { AppProps, NextWebVitalsMetric } from 'next/app';
+import { GoogleAnalytics, event, usePageViews } from 'nextjs-google-analytics';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../components/chakra-theme';
 import { Context } from '../components/Context';
 import './global.css';
 
+// https://github.com/MauricioRobayo/nextjs-google-analytics#web-vitals
+export function reportWebVitals({
+  id,
+  name,
+  label,
+  value,
+}: NextWebVitalsMetric) {
+  event(name, {
+    category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    label: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  });
+}
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  usePageViews();
+
   return (
-    <ChakraProvider theme={theme}>
-      <Context>
-        <Component {...pageProps} />
-      </Context>
-    </ChakraProvider>
+    <>
+      <GoogleAnalytics />
+      <ChakraProvider theme={theme}>
+        <Context>
+          <Component {...pageProps} />
+        </Context>
+      </ChakraProvider>
+    </>
   );
 };
 
