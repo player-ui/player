c359883d53b4d9fe8bc36947e6e5397ac917be4a
Harris Borawski <harris_borawski@intuit.com>
2022-07-25 15:11:58 -0700

    fix path again

--- scripts/specLint.sh
+++ scripts/specLint.sh
@@ -4,8 +4,11 @@
 # Fetch artifacts from this build
 ./fetchArtifacts.sh > /tmp/$CIRCLE_BUILD_NUMBER/artifacts.json
 
+ls -la /tmp
+
 # Find the pod zip url
 export CIRCLE_CI_ZIP=$(./parseArtifactJson.js /tmp/$CIRCLE_BUILD_NUMER/artifacts.json)
 
+echo $CIRCLE_CI_ZIP
 
 bundle exec pod spec lint ../PlayerUI.podspec
