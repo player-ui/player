2889d7f631ffdd4b595447e00bc18e28346a3fbd
Harris Borawski <harrisborawski@gmail.com>
2022-07-27 12:17:11 -0700

    CocoaPod publishing (#20)
    Co-authored-by: Adam Dierkens <adierkens@users.noreply.github.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -23,6 +23,23 @@ for pkg in $PKG_NPM_LABELS ; do
   bazel run --config=release -- ${pkg}.publish --access public --tag ${NPM_TAG}
 done
 
+
+# Fetch artifacts from the iOS stage of this build
+# CIRCLE_BUILD_NUMBER is for the iOS Stage of the build
+# and populated by a .ios-build-number file that is written in that stage
+mkdir -p /tmp/$CIRCLE_BUILD_NUMBER
+./fetchArtifacts.sh > /tmp/$CIRCLE_BUILD_NUMBER/artifacts.json
+
+# Find the pod zip url
+export CIRCLE_CI_ZIP=$(./parseArtifactJson.js /tmp/$CIRCLE_BUILD_NUMBER/artifacts.json)
+
+# Rebuild to stamp the release pod
+bazel build //:PlayerUI_Pod
+
+# Push the podspec to cocoapods, verifying against the zip in the iOS stage artifacts
+# so there is a URL to verify
+bazel run //:PlayerUI_Pod_Push
+
 # Running this here because it will still have the pre-release version in the VERSION file before auto cleans it up
 bazel run --config=release //docs:deploy_docs
 
