6dd9a9da6cd93af4d5e7aa3909b66800ead29266
Harris Borawski <harris_borawski@intuit.com>
2022-08-17 10:34:47 -0700

    define properties in autorc that i was relying on

--- scripts/after-shipit-pod-push.js
+++ scripts/after-shipit-pod-push.js
@@ -3,16 +3,17 @@
 /* eslint-disable no-await-in-loop */
 const { execSync } = require('child_process');
 
-class AfterReleasePodPush {
-  name = 'after-release-pod-push';
+class AfterShipItPodPush {
+  name = 'after-shipit-pod-push';
 
   apply(auto) {
-    auto.hooks.afterRelease.tapPromise(this.name, async ({ dryRun, newVersion }) => {
+    auto.hooks.afterShipIt.tapPromise(this.name, async ({ dryRun, newVersion }) => {
       if (dryRun) {
         auto.logger.log.info(`Dry run: would have pushed pod to trunk`);
       } else {
         let found = false
         let attempt = 0
+
         while (!found && attempt < 10) {
           const { data } = await auto.git.github.repos.listReleases({owner: auto.config.owner, repo: auto.config.repo})
           const release = data.find(element => element.name === newVersion)
@@ -27,10 +28,11 @@ class AfterReleasePodPush {
             })
           }
         }
+
         execSync('bazel run --config=release //:PlayerUI_Pod_Push')
       }
     });
   }
 }
 
-module.exports = AfterReleasePodPush;
+module.exports = AfterShipItPodPush;
