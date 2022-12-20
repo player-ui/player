b10106d0624d4186f91cdd5c696e77429596bc15
Ketan Reddy <ketan_reddy@intuit.com>
2022-12-20 15:30:14 -0800

    fix old import

--- plugins/common-expressions/core/src/expressions/index.ts
+++ plugins/common-expressions/core/src/expressions/index.ts
@@ -1,7 +1,7 @@
 import type {
   ExpressionHandler,
   ExpressionContext,
-} from '@player-ui/expressions';
+} from '@player-ui/player';
 import { withoutContext } from '@player-ui/player';
 import type { Binding } from '@player-ui/types';
 import { toNum } from './toNum';
