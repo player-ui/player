ef775b0e2bc97706b4788aacac9af5d1533c7d92
manansharma18 <manansharma18@gmail.com>
2023-10-18 13:55:30 -0400

    add Support For Segmented Assets

--- plugins/reference-assets/core/src/assets/info/types.ts
+++ plugins/reference-assets/core/src/assets/info/types.ts
@@ -1,4 +1,5 @@
 import type { AssetWrapper, Asset } from '@player-ui/player';
+import type { ActionAsset } from '../action/types';
 
 export interface InfoAsset extends Asset<'info'> {
   /** The string value to show */
@@ -12,4 +13,20 @@ export interface InfoAsset extends Asset<'info'> {
 
   /** List of actions to show at the bottom of the page */
   actions?: Array<AssetWrapper>;
+}
+
+export interface InfoAssetTransform extends InfoAsset {
+  /**
+   *
+   */
+  segmentedActions?: {
+    /**
+     *
+     */
+    next: Array<AssetWrapper<ActionAsset>>;
+    /**
+     *
+     */
+    prev: Array<AssetWrapper<ActionAsset>>;
+  };
 }
