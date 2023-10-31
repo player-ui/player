e67f6886185c343401ae7058bce04c6d88cba398
zwu01 <nancy_wu1@intuit.com>
2023-10-31 10:50:10 -0400

    link flows from plugins to ios mockflows

--- ios/packages/demo/scripts/generateFlowSections.js
+++ ios/packages/demo/scripts/generateFlowSections.js
@@ -5,15 +5,31 @@ const child_process = require('child_process')
 const { camelCase } = require('change-case')
 const { makeFlow } = require('@player-ui/make-flow')
 
-const mocksDir = '../../../../plugins/reference-assets/mocks'
-const assetTypes = fs.readdirSync(mocksDir, { withFileTypes: true })
+const assetsMocksDir = '../../../../plugins/reference-assets/mocks'
+const assetTypes = fs.readdirSync(assetsMocksDir, { withFileTypes: true })
   .filter(type => type.isDirectory())
   .map(type => type.name)
 
-const sections = assetTypes.reduce((acc, currentType) => {
-  const filePaths = child_process.execSync(`find ${mocksDir}/${currentType} -name '*.json'`).toString().trim().split('\n').filter(p => p !== '')
+const assetSections = assetTypes.reduce((acc, currentType) => {
+  const filePaths = child_process.execSync(`find ${assetsMocksDir}/${currentType} -name '*.json'`).toString().trim().split('\n').filter(p => p !== '')
   return {...acc, [currentType]: filePaths}
 }, {})
+
+const pluginsDir = '../../../../plugins'
+
+let pluginTypes = fs.readdirSync(pluginsDir).filter(function (plugin) {
+  return plugin != "reference-assets" ? fs.existsSync(pluginsDir + '/' + plugin + '/mocks') : false
+}).map(type => type)
+
+const pluginSections = pluginTypes.reduce((acc, currentType) => {
+  const filePaths = child_process.execSync(`find ../../../../plugins/${currentType}/mocks -name '*.json'`).toString().trim().split('\n').filter(p => p !== '')
+  return {...acc, [currentType]: filePaths}
+}, {})
+
+const mergedSections = {
+  ...assetSections,
+  ...pluginSections
+}
 
 const toFlowTuple = (flowPath, section) => {
   const { name } = path.parse(flowPath)
@@ -21,7 +37,7 @@ const toFlowTuple = (flowPath, section) => {
 }
 
 const toFlowSection = (section, indent) => {
-  const flows = sections[section]
+  const flows = mergedSections[section]
   return `${indent}(title: "${section}", flows: [
 ${indent}    ${flows.map(flowPath => toFlowTuple(flowPath, section)).join(`,\n${indent}    `)}
 ${indent}])`
@@ -40,8 +56,8 @@ const output = `// swiftlint:disable all
 import PlayerUI
 
 public struct MockFlows {
-    ${Object.keys(sections)
-      .reduce((flows, section) => [...flows, ...sections[section]], [])
+    ${Object.keys(mergedSections)
+      .reduce((flows, section) => [...flows, ...mergedSections[section]], [])
       .map(flowPath => getFlowContents(flowPath))
       .map(({name, json}) => `static let ${name}: String = """
 ${JSON.stringify(json, null, 2).replace(/\\/g, '\\\\')}
@@ -49,7 +65,7 @@ ${JSON.stringify(json, null, 2).replace(/\\/g, '\\\\')}
       .join('\n')
     }
     public static let sections: [FlowLoader.FlowSection] = [
-${Object.keys(sections).map(section => toFlowSection(section, "        ")).join(',\n')}
+${Object.keys(mergedSections).map(section => toFlowSection(section, "        ")).join(',\n')}
     ]
 }
 `
