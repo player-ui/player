971078065c4dba59a8f6d80ffc3969b16c749333
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2023-11-16 21:36:11 -0500

    [jvm#240]: fix testutils node

--- jvm/testutils/src/main/kotlin/com/intuit/player/jvm/testutils/Node.kt
+++ jvm/testutils/src/main/kotlin/com/intuit/player/jvm/testutils/Node.kt
@@ -3,10 +3,9 @@ package com.intuit.player.jvm.testutils
 import com.intuit.player.jvm.core.asset.Asset
 import com.intuit.player.jvm.core.bridge.Invokable
 import com.intuit.player.jvm.core.bridge.runtime.Runtime
-import kotlinx.serialization.ContextualSerializer
+import com.intuit.player.jvm.core.bridge.serialization.serializers.GenericSerializer
 import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.builtins.MapSerializer
-import kotlinx.serialization.builtins.nullable
 import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.json.Json
 import kotlin.reflect.jvm.reflect
@@ -31,13 +30,13 @@ public class Node(private val map: Map<String, Any?>) : com.intuit.player.jvm.co
     }
 
     override fun <T> getSerializable(key: String, deserializer: DeserializationStrategy<T>): T = get(key)?.let { value ->
-        Json.decodeFromJsonElement(deserializer, Json.encodeToJsonElement(ContextualSerializer(Any::class).nullable, value))
+        Json.decodeFromJsonElement(deserializer, Json.encodeToJsonElement(GenericSerializer(), value))
     }!!
 
     override fun <T> deserialize(deserializer: DeserializationStrategy<T>): T =
         Json.decodeFromJsonElement(
             deserializer,
-            Json.encodeToJsonElement(MapSerializer(String.serializer(), ContextualSerializer(Any::class).nullable), map)
+            Json.encodeToJsonElement(MapSerializer(String.serializer(), GenericSerializer()), map)
         )
 
     override fun isReleased(): Boolean = false
