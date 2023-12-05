338165baa6c80d8dc834c59bce24d81c70b33910
hborawski <Harris_Borawski@intuit.com>
2023-12-05 10:18:38 -0800

    fix bundle access

--- plugins/metrics/swiftui/Sources/MetricsPlugin.swift
+++ plugins/metrics/swiftui/Sources/MetricsPlugin.swift
@@ -48,12 +48,16 @@ class RequestTimeWebPlugin: JSBasePlugin {
     }
 
     override open func getUrlForFile(fileName: String) -> URL? {
+        #if SWIFT_PACKAGE
+        ResourceUtilities.urlForFile(name: fileName, ext: "js", bundle: Bundle.module)
+        #else
         ResourceUtilities.urlForFile(
             name: fileName,
             ext: "js",
             bundle: Bundle(for: MetricsPlugin.self),
             pathComponent: "MetricsPlugin.bundle"
         )
+        #endif
     }
 }
 /**
