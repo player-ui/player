52d618aa3afb08b0273935d2654fe83a0d65f755
hborawski <Harris_Borawski@intuit.com>
2023-12-05 12:02:31 -0800

    pull in latest demo app files from main

--- ios/demo/Sources/App.swift
+++ ios/demo/Sources/App.swift
@@ -17,46 +17,6 @@ import PlayerUISwiftUIPendingTransactionPlugin
 import PlayerUITransitionPlugin
 import PlayerUITypesProviderPlugin
 
-let flow = """
-{
-  "id": "generated-flow",
-  "views": [
-    {
-      "id": "action",
-      "type": "action",
-      "exp": "{{count}} = {{count}} + 1",
-      "label": {
-        "asset": {
-          "id": "action-label",
-          "type": "text",
-          "value": "Clicked {{count}} times"
-        }
-      }
-    }
-  ],
-  "data": {
-    "count": 0
-  },
-  "navigation": {
-    "BEGIN": "FLOW_1",
-    "FLOW_1": {
-      "startState": "VIEW_1",
-      "VIEW_1": {
-        "state_type": "VIEW",
-        "ref": "action",
-        "transitions": {
-          "*": "END_Done"
-        }
-      },
-      "END_Done": {
-        "state_type": "END",
-        "outcome": "done"
-      }
-    }
-  }
-}
-"""
-
 @main
 struct BazelApp: App {
     var body: some Scene {
@@ -91,8 +51,16 @@ struct PlayerView: View {
         SwiftUIPendingTransactionPlugin<PendingTransactionPhases>()
     ]
     var body: some View {
-        VStack {
-            SwiftUIPlayer(flow: flow, plugins: plugins, result: .constant(nil))
+        NavigationView {
+            SegmentControlView(
+                plugins: plugins,
+                assetSections: MockFlows.assetSections,
+                pluginSections: MockFlows.pluginSections,
+                completion: { _ in
+                  print("finished")
+                }
+            )
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
