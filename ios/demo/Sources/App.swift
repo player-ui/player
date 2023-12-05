eb02233ebb7b610826aeee600c34fa43d4f5cb7d
hborawski <Harris_Borawski@intuit.com>
2023-12-05 11:14:43 -0800

    update demo with more plugins

--- ios/demo/Sources/App.swift
+++ ios/demo/Sources/App.swift
@@ -1,8 +1,20 @@
 import SwiftUI
+import PlayerUI
 import PlayerUILogger
 import PlayerUISwiftUI
 import PlayerUIReferenceAssets
+
+import PlayerUIBaseBeaconPlugin
 import PlayerUIBeaconPlugin
+import PlayerUICommonExpressionsPlugin
+import PlayerUICommonTypesPlugin
+import PlayerUIExpressionPlugin
+import PlayerUIExternalActionPlugin
+import PlayerUIMetricsPlugin
+import PlayerUIPrintLoggerPlugin
+import PlayerUIPubSubPlugin
+import PlayerUISwiftUIPendingTransactionPlugin
+import PlayerUITypesProviderPlugin
 
 let flow = """
 {
@@ -56,9 +68,30 @@ struct BazelApp: App {
 }
 
 struct PlayerView: View {
+    let plugins: [NativePlugin] = [
+        PrintLoggerPlugin(level: .trace),
+        ReferenceAssetsPlugin(),
+        CommonTypesPlugin(),
+        ExpressionPlugin(),
+        CommonExpressionsPlugin(),
+        ExternalActionPlugin(handler: { _, _, _ in
+            print("external state")
+        }),
+        MetricsPlugin { timing, render, flow in
+            print(timing as Any)
+            print(render as Any)
+            print(flow as Any)
+        },
+        RequestTimePlugin { 5 },
+        PubSubPlugin([]),
+        TypesProviderPlugin(types: [], validators: [], formats: []),
+//        TransitionPlugin(popTransition: .pop),
+        BeaconPlugin<DefaultBeacon> { print(String(describing: $0)) },
+        SwiftUIPendingTransactionPlugin<PendingTransactionPhases>()
+    ]
     var body: some View {
         VStack {
-            SwiftUIPlayer(flow: flow, plugins: [ReferenceAssetsPlugin()], result: .constant(nil))
+            SwiftUIPlayer(flow: flow, plugins: plugins, result: .constant(nil))
         }
     }
 }
