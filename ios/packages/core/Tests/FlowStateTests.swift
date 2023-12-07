219e38e26345262cd47f35bb3c30759eb10a3a58
zwu01 <nancy_wu1@intuit.com>
2023-12-07 12:28:49 -0500

    fix test

--- ios/packages/core/Tests/FlowStateTests.swift
+++ ios/packages/core/Tests/FlowStateTests.swift
@@ -21,7 +21,7 @@ class FlowStateTests: XCTestCase {
         XCTAssertNotNil(inProgress.controllers?.flow.current?.currentState?.value as? NavigationFlowViewState)
         let view = inProgress.controllers?.flow.current?.currentState?.value as? NavigationFlowViewState
 
-        XCTAssertEqual(view?.attributes?["test"], "value")
+        XCTAssertEqual(view?.attributes?["test"] as? String, "value")
     }
 
     func testExternalFlowState() {
