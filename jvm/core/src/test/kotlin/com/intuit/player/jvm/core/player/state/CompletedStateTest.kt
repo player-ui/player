b4b4979f2cc89f4cebcd115597f8c4b318995527
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2024-01-23 20:03:56 -0500

    fix //jvm/core:test

--- jvm/core/src/test/kotlin/com/intuit/player/jvm/core/player/state/CompletedStateTest.kt
+++ jvm/core/src/test/kotlin/com/intuit/player/jvm/core/player/state/CompletedStateTest.kt
@@ -6,6 +6,7 @@ import com.intuit.player.jvm.core.bridge.Node
 import com.intuit.player.jvm.core.bridge.getInvokable
 import com.intuit.player.jvm.core.bridge.runtime.Runtime
 import com.intuit.player.jvm.core.bridge.serialization.format.RuntimeFormat
+import com.intuit.player.jvm.core.data.DataController
 import com.intuit.player.jvm.core.data.DataModelWithParser
 import com.intuit.player.jvm.core.flow.Flow
 import com.intuit.player.jvm.core.player.PlayerFlowStatus
@@ -23,6 +24,10 @@ internal class CompletedStateTest : NodeBaseTest() {
         CompletedState(node)
     }
 
+    private val controllerState by lazy {
+        ControllerState(node)
+    }
+
     @MockK
     private lateinit var mockDataModel: Node
 
@@ -35,6 +40,10 @@ internal class CompletedStateTest : NodeBaseTest() {
         every { node.runtime } returns runtime
         every { runtime.containsKey("getSymbol") } returns true
         every { runtime.getInvokable<String?>("getSymbol") } returns Invokable { "Symbol(hello)" }
+        every { node.getObject("controllers") } returns node
+        every { node.getSerializable<Any>("controllers", any()) } returns controllerState
+        every { node.getObject("data") } returns node
+        every { node.getSerializable<Any>("data", any()) } returns DataController(node)
         every { node.getSerializable("flow", Flow.serializer()) } returns Flow("flowId")
         every { node.getSerializable("dataModel", DataModelWithParser.serializer()) } returns DataModelWithParser(node)
         every { node.getObject("dataModel") } returns mockDataModel
