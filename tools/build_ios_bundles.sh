a7c7c81c0db349c49a8be44d30e20a6ec1138e35
hborawski <Harris_Borawski@intuit.com>
2024-03-19 11:48:22 -0700

    update build_ios_bundles.sh to make sure make-flow is linked right

--- tools/build_ios_bundles.sh
+++ tools/build_ios_bundles.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 git_root=$(git rev-parse --show-toplevel)
 
+# Build all bundle targets that we rely on
 "bazel" build `bazel query 'attr(name, "^.*_Bundles$", //...)' --output label 2>/dev/null`
+# explicitly build make-flow
+"bazel" build //core/make-flow/...
+# run yarn so make-flow is importable in node runtime
+yarn
+# Run pod install to generate xcworkspace
 cd "$git_root/xcode" && bundle exec pod install
