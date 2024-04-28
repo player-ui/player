0a1e6c89b2bd013a5ff2d4daf7e84473cdf72c17
Aish <aish_kumar@intuit.com>
2024-04-28 19:50:39 -0400

    Adding basic compose mock and test

---
+++ plugins/reference-assets/android/src/androidTest/java/com/intuit/playerui/android/reference/assets/text/TextComposeTest.kt
@@ -0,0 +1,23 @@
+package com.intuit.playerui.android.reference.assets.text
+
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.get
+import com.intuit.playerui.android.reference.assets.R
+import com.intuit.playerui.android.reference.assets.test.AssetTest
+import com.intuit.playerui.android.reference.assets.test.shouldBeView
+import org.junit.Assert
+import org.junit.Test
+
+class TextComposeTest: AssetTest("reference-assets")  {
+
+    @Test
+    fun basic() {
+        launchMock("text-compose-basic")
+
+        val collectionValues = currentView?.findViewById<LinearLayout>(R.id.collection_values) ?: throw AssertionError("current view is null")
+        collectionValues[0].shouldBeView<TextView> {
+            Assert.assertEquals("This is some text...using Jetpack Compose", text.toString())
+        }
+    }
+}
