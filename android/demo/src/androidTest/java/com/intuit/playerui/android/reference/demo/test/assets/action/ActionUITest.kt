52d3d2783e52fb1929cd17b329a297e66c1faed4
brocollie08 <sentony93@gmail.com>
2024-07-08 17:52:54 -0400

    add test back in

--- android/demo/src/androidTest/java/com/intuit/playerui/android/reference/demo/test/assets/action/ActionUITest.kt
+++ android/demo/src/androidTest/java/com/intuit/playerui/android/reference/demo/test/assets/action/ActionUITest.kt
@@ -8,7 +8,6 @@ import com.intuit.playerui.android.reference.demo.test.base.AssetUITest
 import com.intuit.playerui.android.reference.demo.test.base.shouldBePlayerState
 import com.intuit.playerui.android.reference.demo.test.base.waitForViewInRoot
 import com.intuit.playerui.core.player.state.CompletedState
-import com.intuit.playerui.core.player.state.ErrorState
 import com.intuit.playerui.core.player.state.InProgressState
 import com.intuit.playerui.core.player.state.dataModel
 import org.junit.Assert.assertEquals
@@ -45,7 +44,7 @@ class ActionUITest : AssetUITest("reference-assets") {
         }
     }
 
-    //TODO: Fix invalid expression not throwing error in core
+    // TODO: Fix invalid expression not throwing error in core
 //    @Test
 //    fun transitionToEndError() {
 //        launchMock("action-transition-to-end")
