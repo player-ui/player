5fdd5dffa666c080cdd85a77f68502c1261be795
sakuntala_motukuri <sakuntala_motukuri@intuit.com>
2024-07-11 14:24:34 -0400

    code cleanup

--- plugins/async-node/components/src/__tests__/index.test.tsx
+++ plugins/async-node/components/src/__tests__/index.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { test, expect } from "vitest";
 import { render } from "@player-tools/dsl";
-import { AsyncNode } from "../dsl";
+import { AsyncNode } from "../index";
 
 test("works for id prop", async () => {
   expect((await render(<AsyncNode id={"nodeId"} />)).jsonValue).toStrictEqual({
