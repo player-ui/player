e01fab23506997ab0f358b90749cbc9400d90d23
sakuntala_motukuri <sakuntala_motukuri@intuit.com>
2024-07-11 00:19:58 -0400

    fixed few review comments

--- plugins/async-node/components/src/dsl/index.tsx
+++ plugins/async-node/components/src/dsl/index.tsx
@@ -2,21 +2,16 @@ import React from "react";
 import type { ObjectNode } from "react-json-reconciler";
 
 export type AsyncNodeProps = {
-  id?: string;
+  id: string;
 };
 
 export const AsyncNode = React.forwardRef<ObjectNode, AsyncNodeProps>(
-  (props) => {
-    const { id } = props;
-    const Wrapper = React.Fragment;
-
+  ({ id }, ref) => {
     return (
-      <Wrapper>
-        <obj>
-          <property name="id">{id}</property>
-          <property name="async">true</property>
-        </obj>
-      </Wrapper>
+      <obj ref={ref}>
+        <property name="id">{id}</property>
+        <property name="async">true</property>
+      </obj>
     );
   },
 );
