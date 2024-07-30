ffa0485bea01c93775adcf6c5ac32af3d8df99cf
brocollie08 <sentony93@gmail.com>
2024-07-30 13:39:16 -0400

    lint

--- android/demo/src/main/java/com/intuit/playerui/android/reference/demo/ui/base/BasePlayerFragment.kt
+++ android/demo/src/main/java/com/intuit/playerui/android/reference/demo/ui/base/BasePlayerFragment.kt
@@ -30,8 +30,10 @@ abstract class BasePlayerFragment : PlayerFragment() {
     private val currentPlayerCanvas get() = binding.playerCanvas.getChildAt(0)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        if (playerViewModel.isDebug) (playerViewModel.runtime as? com.alexii.j2v8debugger.ScriptSourceProvider)?.let {
-            com.alexii.j2v8debugger.StethoHelper.initializeDebugger(requireContext(), it)
+        if (playerViewModel.isDebug) {
+            (playerViewModel.runtime as? com.alexii.j2v8debugger.ScriptSourceProvider)?.let {
+                com.alexii.j2v8debugger.StethoHelper.initializeDebugger(requireContext(), it)
+            }
         }
         return super.onCreateView(inflater, container, savedInstanceState)
     }
