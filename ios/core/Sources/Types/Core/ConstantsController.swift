056cea72d5d698da7fe10834a9207cb07214e768
Chloe Han <ce_han@intuit.com>
2024-08-12 10:14:16 -0400

    add docstrings to public funcs

--- ios/core/Sources/Types/Core/ConstantsController.swift
+++ ios/core/Sources/Types/Core/ConstantsController.swift
@@ -2,7 +2,13 @@ import JavaScriptCore
 
 public class ConstantsController {
     var constantsController: JSValue?
-
+    
+    /// Function to retrieve constants from the providers store
+    /// - Parameters:
+    ///   - key: Key used for the store access
+    ///   - namespace: Namespace values were loaded under
+    ///   - fallback:Optional - if key doesn't exist in namespace what to return (will return unknown if not provided)
+    /// - Returns: Constant values from store
     public func getConstants<T>(key: Any, namespace: String, fallback: Any? = nil) -> T? {
         if let fallbackValue = fallback {
             let value = self.constantsController?.invokeMethod("getConstants", withArguments: [key, namespace, fallbackValue])
@@ -12,15 +18,24 @@ public class ConstantsController {
             return value?.toString() as? T
         }
     }
-
+    
+    /// Function to add constants to the providers store
+    /// - Parameters:
+    ///   - data: Values to add to the constants store
+    ///   - namespace: Namespace values to be added under
     public func addConstants(data: Any, namespace: String) -> Void {
         self.constantsController?.invokeMethod("addConstants", withArguments: [data, namespace])
     }
-
+    
+    /// Function to set values to temporarily override certain keys in the perminant store
+    /// - Parameters:
+    ///   - data: Values to override store with
+    ///   - namespace: Namespace to override
     public func setTemporaryValues(data: Any, namespace: String) -> Void {
         self.constantsController?.invokeMethod("setTemporaryValues", withArguments: [data, namespace])
     }
     
+    /// Clears any temporary values that were previously set
     public func clearTemporaryValues() -> Void {
         self.constantsController?.invokeMethod("clearTemporaryValues", withArguments: [])
     }
