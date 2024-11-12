af9240b75b42efddb9615d6512edd9bdcd4f2b93
Mrigank Mehta <mrigank_mehta@intuit.com>
2024-11-12 13:22:24 -0500

    Implemented cancelBeacon and buildBeacon hooks for Android

--- plugins/beacon/jvm/src/main/kotlin/com/intuit/playerui/plugins/beacon/BeaconPlugin.kt
+++ plugins/beacon/jvm/src/main/kotlin/com/intuit/playerui/plugins/beacon/BeaconPlugin.kt
@@ -1,20 +1,35 @@
 package com.intuit.playerui.plugins.beacon
 
 import com.intuit.playerui.core.asset.Asset
+import com.intuit.playerui.core.bridge.Invokable
+import com.intuit.playerui.core.bridge.Node
+import com.intuit.playerui.core.bridge.NodeWrapper
 import com.intuit.playerui.core.bridge.getInvokable
+import com.intuit.playerui.core.bridge.hooks.NodeSyncBailHook1
+import com.intuit.playerui.core.bridge.hooks.NodeAsyncWaterfallHook2
 import com.intuit.playerui.core.bridge.runtime.Runtime
 import com.intuit.playerui.core.bridge.runtime.ScriptContext
 import com.intuit.playerui.core.bridge.runtime.add
 import com.intuit.playerui.core.bridge.serialization.serializers.GenericSerializer
+import com.intuit.playerui.core.bridge.serialization.serializers.InvokableSerializer
+import com.intuit.playerui.core.bridge.serialization.serializers.NodeSerializableField
+import com.intuit.playerui.core.bridge.serialization.serializers.NodeWrapperSerializer
 import com.intuit.playerui.core.player.Player
+import com.intuit.playerui.core.player.state.PlayerFlowState
 import com.intuit.playerui.core.plugins.JSPluginWrapper
 import com.intuit.playerui.core.plugins.JSScriptPluginWrapper
 import com.intuit.playerui.core.plugins.Pluggable
 import com.intuit.playerui.core.plugins.findPlugin
 import com.intuit.playerui.plugins.settimeout.SetTimeoutPlugin
 import kotlinx.serialization.Contextual
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.MapSerializer
+import kotlinx.serialization.builtins.nullable
+import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.json.Json
+
+public typealias LoggerType = Map<LogSeverity, Invokable<Unit>>
 
 /**
  * Core beaconing plugin wrapper for the JVM. Beaconing format can be augmented with a wrapped core beaconing plugin passed in as [JSPluginWrapper]s.
@@ -22,6 +37,8 @@ import kotlinx.serialization.json.Json
 public class BeaconPlugin(override val plugins: List<JSPluginWrapper>) : JSScriptPluginWrapper(pluginName, sourcePath = bundledSourcePath), Pluggable {
 
     public constructor(vararg plugins: JSPluginWrapper) : this(plugins.toList())
+
+    public val hooks: Hooks by NodeSerializableField(Hooks.serializer())
 
     override fun apply(runtime: Runtime<*>) {
         SetTimeoutPlugin().apply(runtime)
@@ -68,6 +85,49 @@ public class BeaconPlugin(override val plugins: List<JSPluginWrapper>) : JSScrip
         )
     }
 
+    @Serializable(Hooks.Serializer::class)
+    public class Hooks internal constructor(override val node: Node) : NodeWrapper {
+        /** A hook to build beacon */
+        public val buildBeacon: NodeAsyncWaterfallHook2<Any?, HookArgs>
+                by NodeSerializableField(NodeAsyncWaterfallHook2.serializer(GenericSerializer(), HookArgs.serializer()))
+        /** A hook to cancel beacon */
+        public val cancelBeacon: NodeSyncBailHook1<HookArgs, Boolean>
+            by NodeSerializableField(NodeSyncBailHook1.serializer(HookArgs.serializer(), Boolean.serializer()))
+
+        internal object Serializer : NodeWrapperSerializer<Hooks>(::Hooks)
+    }
+
+    @Serializable(HookArgs.Serializer::class)
+    public class HookArgs internal constructor(override val node: Node) : NodeWrapper {
+        /** The current player state */
+        public val state: PlayerFlowState? by NodeSerializableField(PlayerFlowState.serializer().nullable)
+
+        /** The beacon plugin logger */
+        public val logger: LoggerType by NodeSerializableField(
+            MapSerializer(
+                LogSeverity.serializer(),
+                InvokableSerializer(GenericSerializer()) as KSerializer<Invokable<Unit>>,
+            ),
+        )
+
+        /** The action being performed */
+        public val action: String by NodeSerializableField(String.serializer())
+
+        /** The specific element that the beacon originated from */
+        public val element: String by NodeSerializableField(String.serializer())
+
+        /** The asset firing the beacon */
+        public val asset: Asset by NodeSerializableField(Asset.serializer())
+
+        /** The current view */
+        public val view: Asset? by NodeSerializableField(Asset.serializer().nullable)
+
+        /** Any additional data to attach to the event */
+        public val data: Any? by NodeSerializableField(GenericSerializer())
+
+        internal object Serializer : NodeWrapperSerializer<HookArgs>(::HookArgs)
+    }
+
     private companion object {
         private const val pluginName = "BeaconPlugin"
         private const val bundledSourcePath = "plugins/beacon/core/dist/BeaconPlugin.native.js"
@@ -78,6 +138,11 @@ public class BeaconPlugin(override val plugins: List<JSPluginWrapper>) : JSScrip
         val plugins: List<JSPluginWrapper>,
         val callback: (@Contextual Any?) -> Unit,
     )
+}
+
+@Serializable
+public enum class LogSeverity {
+    TRACE, DEBUG, INFO, WARN, ERROR
 }
 
 /** Convenience getter to find the first [BeaconPlugin] registered to the [Player] */
