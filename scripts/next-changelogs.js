842c79c79a348175c41ea9f2bcb2175957d8c792
Ketan Reddy <ketan@ketanreddy.com>
2024-12-02 15:18:00 -0800

    [skip ci] Add auto plugin for adding next release changelogs to changelog file

---
+++ scripts/next-changelogs.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-await-in-loop */
+const { execSync } = require("child_process");
+
+const getLatestReleaseTag = () => {
+  const tags = execSync("git tag --sort=-creatordate", { encoding: "utf8" });
+  return tags
+    .split("\n")
+    .map((t) => t.trim())
+    .filter(
+      (tag) =>
+        tag.includes("-next.") ||
+        tag.match(/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$/),
+    );
+};
+
+class NextChangelogsPlugin {
+  name = "next-changelogs";
+
+  apply(auto) {
+    auto.hooks.next.tapPromise(this.name, async ({ dryRun }) => {
+      const latestRelease = getLatestReleaseTag();
+
+      if (dryRun) {
+        auto.logger.log.info(`Dry run: making changelog for: ${latestRelease}`);
+      } else {
+        await auto.changelog({ from: latestRelease });
+      }
+    });
+  }
+}
+
+module.exports = NextChangelogsPlugin;
