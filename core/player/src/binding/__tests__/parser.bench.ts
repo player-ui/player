258397ed4921622bef18acf8b5c074939e2f09a8
Ketan Reddy <ketan@ketanreddy.com>
2024-12-19 12:21:45 -0800

    Fix binding instantiation when paths have quoted numbers

--- core/player/src/binding/__tests__/parser.bench.ts
+++ core/player/src/binding/__tests__/parser.bench.ts
@@ -1,6 +1,6 @@
 import { bench, describe } from "vitest";
 import get from "dlv";
-import { getBindingSegments } from "..";
+import { BindingInstance, getBindingSegments } from "..";
 import { testCases, testModel } from "./resolver.test";
 import { parseCustom, ParserSuccessResult } from "../../binding-grammar";
 import { resolveBindingAST } from "../resolver";
@@ -15,6 +15,26 @@ describe("parser benchmarks", () => {
           convertToPath: (p) => p,
           evaluate: () => undefined,
         });
+      });
+    },
+    { iterations: 10000 },
+  );
+});
+
+describe("binding creation benchmarks", () => {
+  testCases.map(
+    ([input, expectedOutput]) => {
+      bench(`Resolving binding: ${input}`, () => {
+        const parsedBinding = parseCustom(input);
+        const result = resolveBindingAST(
+          (parsedBinding as ParserSuccessResult).path,
+          {
+            getValue: (path) => get(testModel, getBindingSegments(path) as any),
+            convertToPath: (p) => p,
+            evaluate: () => undefined,
+          },
+        );
+        const bi = new BindingInstance(result.path);
       });
     },
     { iterations: 10000 },
