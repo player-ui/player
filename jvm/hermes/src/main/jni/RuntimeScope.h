39592d3bde515183e02fb8005ff2d1de899f6f28
brocollie08 <sentony93@gmail.com>
2025-01-02 18:48:56 -0500

    Hermes reference tracking (#563)
    * explicit jhybrid class deconstructors
    
    * move ownership of values to runtime
    
    * fix isReleased
    
    * runtime approach
    
    * referenceTracker
    
    * rename to RuntimeScope
    
    * .
    
    * somewhat working
    
    * debugging + trackRef
    
    * sort of working again
    
    * ok
    
    * do not include jni by default
    
    * remove all the debug logs
    
    * working without passing runtime for undefined/null
    
    * reverted null/undefined now that runtime isn't needed and no longer tracked
    
    * address some PR comments
    
    * addressed error handling and null ptr
    
    * revert primitive tracking back
    
    * clear the scope maps after release
    
    * filter tracking in scoped Value constructor too
    
    * object deconstructor
    
    ---------
    
    Co-authored-by: Jeremiah Zucker <jeremiah_zucker@intuit.com>

---
+++ jvm/hermes/src/main/jni/RuntimeScope.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <jsi/jsi.h>
+#include <unordered_map>
+#include <variant>
+
+using namespace std;
+using namespace facebook::jsi;
+
+namespace intuit::playerui {
+// Define the supported types for the variant
+using VariantType = variant<Value, Object, Array, Function, Symbol>;
+
+class RuntimeScope {
+public:
+    unique_ptr<unordered_map<void*, unique_ptr<Value>>> valueScope;
+    unique_ptr<unordered_map<void*, unique_ptr<Object>>> objectScope;
+    unique_ptr<unordered_map<void*, unique_ptr<Array>>> arrayScope;
+    unique_ptr<unordered_map<void*, unique_ptr<Function>>> functionScope;
+    unique_ptr<unordered_map<void*, unique_ptr<Symbol>>> symbolScope;
+
+    void trackValue(void* ptr, Value value);
+
+    void trackFunction(void* ptr, Function value);
+
+    void trackObject(void* ptr, Object value);
+
+    void trackArray(void* ptr, Array value);
+
+    void trackSymbol(void* ptr, Symbol value);
+
+    Value* getValue(void* ptr);
+
+    Function* getFunction(void* ptr);
+
+    Array* getArray(void* ptr);
+
+    Object* getObject(void* ptr);
+
+    Symbol* getSymbol(void* ptr);
+
+    void clearRef(void* ptr);
+
+    explicit RuntimeScope(): valueScope(make_unique<unordered_map<void*, unique_ptr<Value>>>()), objectScope(make_unique<unordered_map<void*, unique_ptr<Object>>>()), arrayScope(make_unique<unordered_map<void*, unique_ptr<Array>>>()), functionScope(make_unique<unordered_map<void*, unique_ptr<Function>>>()), symbolScope(make_unique<unordered_map<void*, unique_ptr<Symbol>>>()){}
+};
+}
