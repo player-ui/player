549c62edb306d9a51e37ae5b5ee625e7a5523fa1
brocollie08 <sentony93@gmail.com>
2025-01-29 15:18:40 -0500

    remove outdated functions
    Signed-off-by: brocollie08 <sentony93@gmail.com>

--- jvm/hermes/src/main/jni/RuntimeScope.cpp
+++ jvm/hermes/src/main/jni/RuntimeScope.cpp
@@ -5,7 +5,6 @@
 
 namespace intuit::playerui {
 
-//template<typename T>
 std::weak_ptr<Value> RuntimeScope::trackValue(Value value) {
     std::shared_ptr<Value> sp = make_shared<Value>(std::move(value));
     valueScope->insert(sp);
@@ -40,78 +39,5 @@ std::weak_ptr<facebook::jsi::Symbol> RuntimeScope::trackSymbol(facebook::jsi::Sy
     weak_ptr wp = sp;
     return wp;
 }
-
-Value* RuntimeScope::getValue(void* ptr) {
-/*    auto it = valueScope->find(ptr);
-    if (it != valueScope->end()) {
-        return valueScope->at(ptr).get();
-    }
-    return nullptr;*/
-}
-
-Object* RuntimeScope::getObject(void *ptr) {
-/*    if (objectScope->find(ptr) != objectScope->end()) {
-        return objectScope->at(ptr).get();
-    } else if (functionScope->find(ptr) != functionScope->end()) {
-        return functionScope->at(ptr).get();
-    } else if (arrayScope->find(ptr) != arrayScope->end()) {
-        return arrayScope->at(ptr).get();
-    }*/
-    return nullptr;
-}
-
-Function* RuntimeScope::getFunction(void *ptr) {
-/*    auto it = functionScope->find(ptr);
-    if (it != functionScope->end()) {
-        return functionScope->at(ptr).get();
-    }*/
-    return nullptr;
-}
-
-Array* RuntimeScope::getArray(void *ptr) {
-/*    auto it = arrayScope->find(ptr);
-    if (it != arrayScope->end()) {
-        return arrayScope->at(ptr).get();
-    }*/
-    return nullptr;
-}
-
-Symbol* RuntimeScope::getSymbol(void *ptr) {
-/*    auto it = symbolScope->find(ptr);
-    if (it != symbolScope->end()) {
-        return symbolScope->at(ptr).get();
-    }*/
-    return nullptr;
-}
-
-/*void RuntimeScope::clearSymbol(weak_ptr<Symbol> ptr) {
-    if (symbolScope->find(ptr) != symbolScope->end()) {
-        symbolScope->erase(ptr);
-    }
-}
-
-void RuntimeScope::clearObject(weak_ptr<Object> ptr) {
-    if (objectScope->find(ptr) != objectScope->end()) {
-        objectScope->erase(ptr);
-    }
-}
-
-void RuntimeScope::clearArray(weak_ptr<Array> ptr) {
-    if (arrayScope->find(ptr) != arrayScope->end()) {
-        arrayScope->erase(ptr);
-    }
-}
-
-void RuntimeScope::clearValue(weak_ptr<Value> ptr) {
-    if (valueScope->find(ptr) != valueScope->end()) {
-        valueScope->erase(ptr);
-    }
-}
-
-void RuntimeScope::clearFunction(weak_ptr<Function> ptr) {
-    if (functionScope->find(ptr) != functionScope->end()) {
-        functionScope->erase(ptr);
-    }
-}*/
 
 }
--- jvm/hermes/src/main/jni/RuntimeScope.h
+++ jvm/hermes/src/main/jni/RuntimeScope.h
@@ -9,8 +9,6 @@ using namespace std;
 using namespace facebook::jsi;
 
 namespace intuit::playerui {
-// Define the supported types for the variant
-using VariantType = variant<Value, Object, Array, Function, Symbol>;
 
 class RuntimeScope {
 public:
@@ -29,26 +27,6 @@ public:
     std::weak_ptr<Array> trackArray(Array value);
 
     std::weak_ptr<Symbol> trackSymbol(Symbol value);
-
-    Value* getValue(void* ptr);
-
-    Function* getFunction(void* ptr);
-
-    Array* getArray(void* ptr);
-
-    Object* getObject(void* ptr);
-
-    Symbol* getSymbol(void* ptr);
-
-/*    void clearSymbol(weak_ptr<Symbol> ptr);
-
-    void clearObject(weak_ptr<Object> ptr);
-
-    void clearArray(weak_ptr<Array> ptr);
-
-    void clearValue(weak_ptr<Value> ptr);
-
-    void clearFunction(weak_ptr<Function> ptr);*/
 
     explicit RuntimeScope(): valueScope(make_unique<set<shared_ptr<Value>>>()), objectScope(make_unique<set<shared_ptr<Object>>>()), arrayScope(make_unique<set<shared_ptr<Array>>>()), functionScope(make_unique<set<shared_ptr<Function>>>()), symbolScope(make_unique<set<shared_ptr<Symbol>>>()){}
 };
