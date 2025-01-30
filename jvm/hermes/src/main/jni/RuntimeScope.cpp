b1fc57c2c7735cd18b90ede2120173b03a6e31ca
brocollie08 <sentony93@gmail.com>
2025-01-30 16:39:18 -0500

    weakptr type

--- jvm/hermes/src/main/jni/RuntimeScope.cpp
+++ jvm/hermes/src/main/jni/RuntimeScope.cpp
@@ -8,35 +8,35 @@ namespace intuit::playerui {
 std::weak_ptr<Value> RuntimeScope::trackValue(Value value) {
     std::shared_ptr<Value> sp = make_shared<Value>(std::move(value));
     valueScope->insert(sp);
-    std::weak_ptr wp = sp;
+    std::weak_ptr<Value> wp = sp;
     return wp;
 }
 
 std::weak_ptr<Function> RuntimeScope::trackFunction(Function value) {
     std::shared_ptr<Function> sp = make_shared<Function>(std::move(value));
     functionScope->insert(sp);
-    std::weak_ptr wp = sp;
+    std::weak_ptr<Function> wp = sp;
     return wp;
 }
 
-std::weak_ptr<facebook::jsi::Array> RuntimeScope::trackArray(facebook::jsi::Array value) {
-    std::shared_ptr<facebook::jsi::Array> sp = make_shared<facebook::jsi::Array>(std::move(value));
+std::weak_ptr<Array> RuntimeScope::trackArray(Array value) {
+    std::shared_ptr<Array> sp = make_shared<Array>(std::move(value));
     arrayScope->insert(sp);
-    std::weak_ptr wp = sp;
+    std::weak_ptr<Array> wp = sp;
     return wp;
 }
 
-std::weak_ptr<facebook::jsi::Object> RuntimeScope::trackObject(facebook::jsi::Object value) {
-    std::shared_ptr<facebook::jsi::Object> sp = make_shared<facebook::jsi::Object>(std::move(value));
+std::weak_ptr<Object> RuntimeScope::trackObject(Object value) {
+    std::shared_ptr<Object> sp = make_shared<Object>(std::move(value));
     objectScope->insert(sp);
-    std::weak_ptr wp = sp;
+    std::weak_ptr<Object> wp = sp;
     return wp;
 }
 
-std::weak_ptr<facebook::jsi::Symbol> RuntimeScope::trackSymbol(facebook::jsi::Symbol value) {
-    std::shared_ptr<facebook::jsi::Symbol> sp = make_shared<facebook::jsi::Symbol>(std::move(value));
+std::weak_ptr<Symbol> RuntimeScope::trackSymbol(Symbol value) {
+    std::shared_ptr<Symbol> sp = make_shared<Symbol>(std::move(value));
     symbolScope->insert(sp);
-    std::weak_ptr wp = sp;
+    std::weak_ptr<Symbol> wp = sp;
     return wp;
 }
 
