ea731525b6d7061f359ae3f1fe1c948e7c116e0c
Chloe Han <ce_han@intuit.com>
2025-01-30 12:28:02 -0500

    add missing type

--- plugins/reference-assets/core/src/assets/chat-message-wrapper/types.ts
+++ plugins/reference-assets/core/src/assets/chat-message-wrapper/types.ts
@@ -1,6 +1,9 @@
 import type { AssetWrapper, Asset } from "@player-ui/player";
 
 export interface ChatMessageWrapperAsset extends Asset<"chat-message-wrapper"> {
-  /** The string value to show */
   values?: Array<AssetWrapper> | Array<Asset>;
+}
+export interface TransformedChatMessageWrapperAsset
+  extends ChatMessageWrapperAsset {
+  values?: Array<AssetWrapper>;
 }
