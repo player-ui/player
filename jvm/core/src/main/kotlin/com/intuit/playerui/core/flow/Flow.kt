a26436d42911e5beb21ff92318522be76b4693ca
brocollie08 <sentony93@gmail.com>
2025-02-25 17:28:22 -0500

    map backing flow

--- jvm/core/src/main/kotlin/com/intuit/playerui/core/flow/Flow.kt
+++ jvm/core/src/main/kotlin/com/intuit/playerui/core/flow/Flow.kt
@@ -1,7 +1,9 @@
 package com.intuit.playerui.core.flow
 
+import com.intuit.playerui.core.bridge.EmptyNode
 import com.intuit.playerui.core.bridge.Node
 import com.intuit.playerui.core.bridge.NodeWrapper
+import com.intuit.playerui.core.bridge.getSerializable
 import com.intuit.playerui.core.bridge.runtime.Runtime
 import com.intuit.playerui.core.bridge.serialization.format.serializer
 import com.intuit.playerui.core.bridge.serialization.serializers.NodeSerializableField
@@ -19,56 +21,54 @@ import kotlinx.serialization.json.JsonNull
 /** Structure shaping the JSON payload the player expects */
 @Serializable(with = Flow.Serializer::class)
 public class Flow(override val node: Node) : NodeWrapper {
-    @OptIn(ExperimentalPlayerApi::class)
-    public val id: String by NodeSerializableField(String.serializer())
 
-    @OptIn(ExperimentalPlayerApi::class)
-    public val views: List<Node>? by NodeSerializableField(ListSerializer(Node.serializer()).nullable)
+    private var mFlow: MutableMap<String, Any?>? = null
 
-    @OptIn(ExperimentalPlayerApi::class)
-    public val schema: Node? by NodeSerializableField(Node.serializer().nullable)
+    private constructor(
+        id: String = UNKNOWN_ID,
+        views: List<Node>? = emptyList(),
+        schema: Node? = null,
+        data: Node? = null,
+        navigation: Navigation? = null
+    ) : this(EmptyNode) {
+        this.mFlow = mutableMapOf<String, Any?>().also {
+            it["id"] = id
+            it["views"] = views
+            it["schema"] = schema
+            it["data"] = data
+            it["navigation"] = navigation
+        }
+    }
 
-    @OptIn(ExperimentalPlayerApi::class)
-    public val data: Node? by NodeSerializableField(Node.serializer().nullable)
+    public val id: String
+        get() = mFlow?.let {
+            it["id"] as String
+        } ?: node.getString("id") ?: UNKNOWN_ID
 
-    @OptIn(ExperimentalPlayerApi::class)
-    public val navigation: Navigation? by NodeSerializableField(Navigation.serializer().nullable)
+    public val views: List<Node>?
+        get() = mFlow?.let {
+            it["views"] as? List<Node>
+        } ?: node.getList("views") as? List<Node>
+
+    public val schema: Node?
+        get() = mFlow?.let {
+            it["schema"] as? Node
+        } ?: node.getObject("schema")
+
+    public val data: Node?
+        get() = mFlow?.let {
+            it["data"] as? Node
+        } ?: node.getObject("data")
+
+    public val navigation: Navigation?
+        get() = mFlow?.let {
+            it["navigation"] as? Navigation
+        } ?: node.getSerializable("navigation")
 
     internal object Serializer : NodeWrapperSerializer<Flow>(::Flow)
 
     public companion object {
         public const val UNKNOWN_ID: String = "unknown-id"
-//        public operator fun invoke(
-//            id: String = UNKNOWN_ID,
-//            views: List<JsonElement>? = emptyList(),
-//            schema: JsonElement = JsonNull,
-//            data: JsonElement = JsonNull,
-//            navigation: Navigation? = null
-//        ) : Flow {
-//            val paramsMap: Map<String, Any?> = mapOf(
-//                "id" to id,
-//                "views" to views,
-//                "schema" to schema,
-//                "data" to data,
-//                "navigation" to navigation
-//            )
-//            return Flow(object : Node {
-//                override fun <T> getSerializable(key: String, deserializer: DeserializationStrategy<T>): T = throw UnsupportedOperationException()
-//                override fun <T> deserialize(deserializer: DeserializationStrategy<T>): T = throw UnsupportedOperationException()
-//                override val entries: Set<Map.Entry<String, Any?>> = paramsMap.entries
-//                override val keys: Set<String> = paramsMap.keys
-//                override val size: Int = paramsMap.size
-//                override val values: Collection<Any?> = paramsMap.values
-//                override fun containsKey(key: String): Boolean = paramsMap.containsKey(key)
-//                override fun containsValue(value: Any?): Boolean = paramsMap.containsValue(value)
-//                override fun get(key: String): Any? = paramsMap[key]
-//                override fun isEmpty(): Boolean = paramsMap.isEmpty()
-//                override fun isReleased(): Boolean = false
-//                override fun isUndefined(): Boolean = false
-//                override fun nativeReferenceEquals(other: Any?): Boolean = other is Node && this.entries == other.entries
-//                override val runtime: Runtime<*> get() = throw UnsupportedOperationException()
-//            })
-//        }
         public fun createFlow(
             id: String = UNKNOWN_ID,
             views: List<Node>? = emptyList(),
@@ -76,8 +76,7 @@ public class Flow(override val node: Node) : NodeWrapper {
             data: Node? = null,
             navigation: Navigation? = null
         ): Flow {
-            val jsonString = "{\"id\":\"$id\",\"views\":${views},\"schema\":$schema,\"data\":$data,\"navigation\":$navigation}"
-            return Json.decodeFromString(Flow.serializer(), jsonString)
+            return Flow(id, views, schema, data, navigation)
         }
     }
 }
