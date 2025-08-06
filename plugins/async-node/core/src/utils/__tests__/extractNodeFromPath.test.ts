26ad67f57fb7520a1e2a3fb734ee10eaf0b05799
Thomas Marmer <marmerthomas@gmail.com>
2025-08-06 15:30:02 -0400

    add more tests for extractNodeFromPath

--- plugins/async-node/core/src/utils/__tests__/extractNodeFromPath.test.ts
+++ plugins/async-node/core/src/utils/__tests__/extractNodeFromPath.test.ts
@@ -88,4 +88,94 @@ describe("extractNodeFromPath", () => {
       },
     });
   });
+
+  const emptyPaths = [undefined, []];
+  it.each(emptyPaths)(
+    "should return the original node if the path is empty or undefined",
+    (path) => {
+      const node: Node.Value = {
+        type: NodeType.Value,
+        value: {},
+        children: [
+          {
+            path: ["value"],
+            value: {
+              type: NodeType.Value,
+              value: {},
+              children: [
+                {
+                  path: ["asset"],
+                  value: {
+                    type: NodeType.Asset,
+                    value: {
+                      id: "id-2",
+                      type: "type-2",
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      };
+
+      const result = extractNodeFromPath(node, path);
+
+      expect(result).toStrictEqual(node);
+    },
+  );
+
+  const noChildrenNodes: Node.Node[] = [
+    // No children property
+    {
+      id: "test",
+      type: NodeType.Async,
+      value: {
+        type: NodeType.Value,
+        value: {
+          id: "test",
+        },
+      },
+    },
+    // Children explicitly set to undefined
+    {
+      type: NodeType.Value,
+      value: {},
+      children: undefined,
+    },
+  ];
+
+  it.each(noChildrenNodes)(
+    "should return undefined if there are no children in the node",
+    (node) => {
+      const result = extractNodeFromPath(node, ["value", "asset"]);
+      expect(result).toBeUndefined();
+    },
+  );
+
+  it("should return undefined if there is no match", () => {
+    const node: Node.Value = {
+      type: NodeType.Value,
+      children: [
+        {
+          path: ["very", "long", "path"],
+          value: {
+            type: NodeType.Value,
+            value: {},
+          },
+        },
+        {
+          path: ["value", "not-asset"],
+          value: {
+            type: NodeType.Value,
+            value: {},
+          },
+        },
+      ],
+      value: {},
+    };
+
+    const result = extractNodeFromPath(node, ["value", "asset"]);
+    expect(result).toBeUndefined();
+  });
 });
