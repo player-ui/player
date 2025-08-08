30b5143ac1750d5f84ef8a5b83294898b278119e
Thomas Marmer <marmerthomas@gmail.com>
2025-08-08 12:03:44 -0400

    update reference assets test to test with all plugins

--- plugins/reference-assets/core/src/plugin.ts
+++ plugins/reference-assets/core/src/plugin.ts
@@ -6,21 +6,21 @@ import {
   AsyncNodePluginPlugin,
 } from "@player-ui/async-node-plugin";
 
-const metaPlugin = new MetaPlugin([
-  new AsyncNodePlugin({
-    plugins: [new AsyncNodePluginPlugin()],
-  }),
-  new ReferenceAssetsTransformPlugin(),
-  new ChatUiDemoPlugin(),
-]);
-
 /**
  * A plugin to add transforms for the reference assets
  */
 export class ReferenceAssetsPlugin implements PlayerPlugin {
   name = "reference-assets-plugin";
 
+  private readonly metaPlugin = new MetaPlugin([
+    new AsyncNodePlugin({
+      plugins: [new AsyncNodePluginPlugin()],
+    }),
+    new ReferenceAssetsTransformPlugin(),
+    new ChatUiDemoPlugin(),
+  ]);
+
   apply(player: Player): void {
-    player.registerPlugin(metaPlugin);
+    player.registerPlugin(this.metaPlugin);
   }
 }
