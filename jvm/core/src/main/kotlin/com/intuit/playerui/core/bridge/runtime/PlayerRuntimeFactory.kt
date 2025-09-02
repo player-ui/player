ec94c539cba4192f65ad33ee2c14d956ab509a71
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2025-09-02 17:25:34 -0400

    lint fix

--- jvm/core/src/main/kotlin/com/intuit/playerui/core/bridge/runtime/PlayerRuntimeFactory.kt
+++ jvm/core/src/main/kotlin/com/intuit/playerui/core/bridge/runtime/PlayerRuntimeFactory.kt
@@ -33,5 +33,5 @@ public val runtimeContainers: List<PlayerRuntimeContainer> = PlayerRuntimeContai
 
 /** Default [PlayerRuntimeFactory] to use if none are specified */
 public val runtimeFactory: PlayerRuntimeFactory<*> = runtimeContainers.firstOrNull()?.factory ?: error(
-    "Failed to find JS Player runtime implementation in the classpath: consider adding player runtime dependency. $RUNTIME_DETAILS_MSG"
+    "Failed to find JS Player runtime implementation in the classpath: consider adding player runtime dependency. $RUNTIME_DETAILS_MSG",
 )
