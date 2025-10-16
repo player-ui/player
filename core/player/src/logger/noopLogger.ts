2d24c2e459bb2303fb951e112794fcc07afdd087
Dmitriy T <dtychshenko@hotmail.com>
2025-10-16 10:51:39 -0400

    refactor: replace default exports with named exports
    All default exports have been replaced with named exports across core packages, plugins, and React components. Closes #669
    
    Affected packages:
    - `@player-ui/make-flow`: identify function
    - `@player-ui/partial-match-registry`: createObjectMatcher function
    - `@player-ui/player`: Logger classes and view plugins
    - `@player-ui/react`: ReactPlayer component and OnUpdatePlugin
    - `@player-ui/metrics-plugin`: MetricsCorePlugin class
    - `@player-ui/auto-scroll-manager-plugin-react`: scrollIntoViewWithOffset
    
    External dependencies have been verified to be not using the afffected default exports that changed.

--- core/player/src/logger/noopLogger.ts
+++ core/player/src/logger/noopLogger.ts
@@ -4,7 +4,7 @@ import type { Logger } from "./types";
 const noop = () => {};
 
 /** A logger implementation that goes nowhere */
-export default class NoopLogger implements Logger {
+export class NoopLogger implements Logger {
   public readonly trace = noop;
   public readonly debug = noop;
   public readonly info = noop;
