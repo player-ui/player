c0cb723fb0bf04a46103c7c924e318ea69c40ab0
Ketan Reddy <ketan@ketanreddy.com>
2025-10-23 15:06:21 -0700

    break out bundle analysis to separate step to run build of js modules and not block critical path.

--- scripts/bundle-analyze.sh
+++ scripts/bundle-analyze.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 readonly JS_PACKAGES=`bazel query --output=package 'kind("npm_package rule", //...) - attr("tags", "\[.*do-not-publish.*\]", //...)'`
+readonly JS_BUILD_TARGETS=`bazel query --output=label 'kind("ts_project rule", //...) - attr("tags", "\[.*do-not-publish.*\]", //...)'`
+
+bazel build -- $JS_BUILD_TARGETS
 
 for pkg in $JS_PACKAGES ; do
     npx @codecov/bundle-analyzer ./bazel-bin/${pkg}/dist --bundle-name=${pkg} --upload-token=$CODECOV_TOKEN
