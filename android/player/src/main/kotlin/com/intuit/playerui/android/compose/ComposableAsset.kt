e62af6b49ea1efde9dda39d71d9ce322eec178a5
jdang2 <jun_dang@intuit.com>
2026-05-07 17:13:33 -0400

    Revert "bring back import kotlin.collections.emptyList"
    This reverts commit ebbac1b5f3099eccbc5bfd478471376a249bd457.

--- android/player/src/main/kotlin/com/intuit/playerui/android/compose/ComposableAsset.kt
+++ android/player/src/main/kotlin/com/intuit/playerui/android/compose/ComposableAsset.kt
@@ -31,7 +31,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.serialization.KSerializer
 import kotlin.coroutines.cancellation.CancellationException
-import kotlin.collections.emptyList
 
 /**
  * Base class for assets that render using Jetpack Compose.
