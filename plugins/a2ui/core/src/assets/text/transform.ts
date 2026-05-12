582512fbf7d206f8494b0af367fd2af42c96293e
Ketan Reddy <ketan@ketanreddy.com>
2026-05-12 11:34:06 -0700

    Use actual binding regex for text asset transform

--- plugins/a2ui/core/src/assets/text/transform.ts
+++ plugins/a2ui/core/src/assets/text/transform.ts
@@ -1,4 +1,5 @@
 import type { TransformFunction } from "@player-ui/player";
+import { SIMPLE_BINDING_REGEX } from "@player-ui/player";
 import type { TextAsset } from "./types";
 
 //TODO basic check to see if text is just a binding. Make more robust
@@ -7,7 +8,7 @@ export const textTransform: TransformFunction<TextAsset, TextAsset> = (
   options,
 ) => {
   const text =
-    asset.text && asset.text?.includes(".")
+    asset.text && asset.text?.match(SIMPLE_BINDING_REGEX)
       ? options.data.model.get(asset.text)
       : asset.text;
   return {
