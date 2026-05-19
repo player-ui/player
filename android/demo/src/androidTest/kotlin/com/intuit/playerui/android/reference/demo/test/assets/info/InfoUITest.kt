5330879b8790b73ae17983a6b7fb9c52a46d9aeb
brocollie08 <sentony93@gmail.com>
2026-05-19 11:56:58 -0400

    info test use text

--- android/demo/src/androidTest/kotlin/com/intuit/playerui/android/reference/demo/test/assets/info/InfoUITest.kt
+++ android/demo/src/androidTest/kotlin/com/intuit/playerui/android/reference/demo/test/assets/info/InfoUITest.kt
@@ -1,7 +1,7 @@
 package com.intuit.playerui.android.reference.demo.test.assets.info
 
-import androidx.compose.ui.test.onAllNodesWithTag
-import androidx.compose.ui.test.performClick
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -12,10 +12,10 @@ import com.intuit.playerui.core.player.state.InProgressState
 import org.junit.Test
 
 class InfoUITest : ComposeUITest("info") {
-    enum class Action {
-        Next,
-        Dismiss,
-        Continue,
+    enum class Action(val label: String) {
+        Next("Next"),
+        Dismiss("Dismiss"),
+        Continue("Continue"),
     }
 
     fun verifyView(view: Int) {
@@ -26,15 +26,12 @@ class InfoUITest : ComposeUITest("info") {
     fun verifyAndProceed(
         view: Int,
         action: Action? = null,
-        index: Int? = null,
     ) {
         verifyView(view)
 
         action?.let {
-            androidComposeRule
-                .onAllNodesWithTag("action")
-                .get(index ?: 0)
-                .performClick()
+            waitForViewInRoot(withText(it.label))
+            onView(withText(it.label)).perform(click())
         }
     }
 
@@ -42,11 +39,11 @@ class InfoUITest : ComposeUITest("info") {
     fun basic() {
         launchMock("info-modal-flow")
 
-        verifyAndProceed(1, Action.Continue, 0)
-        verifyAndProceed(2, Action.Dismiss, 1)
-        verifyAndProceed(1, Action.Continue, 0)
-        verifyAndProceed(2, Action.Next, 0)
-        verifyAndProceed(3, Action.Next, 0)
+        verifyAndProceed(1, Action.Continue)
+        verifyAndProceed(2, Action.Dismiss)
+        verifyAndProceed(1, Action.Continue)
+        verifyAndProceed(2, Action.Next)
+        verifyAndProceed(3, Action.Next)
         verifyView(1)
 
         player.shouldBeAtState<InProgressState>()
